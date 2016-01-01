700f0366f41a5d4752b4330a8218fb0ab8405fc3
Robbie <Robbie>
2015-12-31 19:19:08 -0600

    Updated spaceship interface semantics

--- Project 2 - Spaceship Interface/spacecraft_interface.ino
+++ Project 2 - Spaceship Interface/spacecraft_interface.ino
@@ -9,31 +9,33 @@ Description:    Code for the science fiction spacecraft interface built with an
 */
 
 void ReadSwitchSensor();
-boolean SwitchIsPressed();
-void TurnGreenLEDOffAndBlinkRedLEDs();
+boolean PushButtonIsPressed();
+void TurnOffGreenLEDAndBlinkRedLEDs();
 void TurnOnGreenLEDAndTurnOffRedLEDs();
 
+const int pushButtonPinNum = 2;
+const int greenLEDPinNum = 3;
+const int redLED1PinNum = 4;
+const int redLED2PinNum = 5;
 const int redLEDBlinkDelay = 250;
 int pushButtonState = 0;
 
 /* Sets up the arduino's digital pins */
 void setup(){
     
-    pinMode(2, INPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
+    pinMode(pushButtonPinNum, INPUT);
+    pinMode(greenLEDPinNum, OUTPUT);
+    pinMode(redLED1PinNum, OUTPUT);
+    pinMode(redLED2PinNum, OUTPUT);
     
 }
 
 /*Continous loop which allows the user to engage hyperdrive*/
 void loop(){
-    
-    ReadSwitchSensor();
-    
-    if(SwitchIsPressed()){
+        
+    if(PushButtonIsPressed()){
      
-        TurnGreenLEDOffAndBlinkRedLEDs();
+        TurnOffGreenLEDAndBlinkRedLEDs();
         
     } else {
         
@@ -43,32 +45,33 @@ void loop(){
     
 }
 
-/* Reads the push button sensor to determine whether is been pressed */
-void ReadSwitchSensor(){
-    
-    pushButtonState = digitalRead(2);
-    
-}
-
 /* Determines if the switch has been pressed */
-boolean SwitchIsPressed(){
+boolean PushButtonIsPressed(){
+    
+    ReadSwitchSensor();
     
     return pushButtonState == HIGH;
     
 }
 
+/* Reads the push button sensor to determine whether is been pressed */
+void ReadSwitchSensor(){
+    
+    pushButtonState = digitalRead(pushButtonPinNum);
+    
+}
 
 /* Turn green LED off and blink red LEDs to make it seem like hyperdrive has been engaged */
-void TurnGreenLEDOffAndBlinkRedLEDs(){
+void TurnOffGreenLEDAndBlinkRedLEDs(){
     
-    digitalWrite(3, LOW);   // Turn off Green LED 1
-    digitalWrite(4, LOW);   // Turn off Red LED 1
-    digitalWrite(5, HIGH);  // Turn on Red LED 2
+    digitalWrite(greenLEDPinNum, LOW);   // Turn off Green LED 1
+    digitalWrite(redLED1PinNum, LOW);   // Turn off Red LED 1
+    digitalWrite(redLED2PinNum, HIGH);  // Turn on Red LED 2
 
     delay(redLEDBlinkDelay);
 
-    digitalWrite(4, HIGH);  // Turn on Red LED 1
-    digitalWrite(5, LOW);   // Turn off Red LED 2
+    digitalWrite(redLED1PinNum, HIGH);  // Turn on Red LED 1
+    digitalWrite(redLED2PinNum, LOW);   // Turn off Red LED 2
 
     delay(redLEDBlinkDelay);     
     
@@ -77,8 +80,8 @@ void TurnGreenLEDOffAndBlinkRedLEDs(){
 /* Turn on the green LED and turn off the red LEDs to make it seem like hyperdrive has been disengaged*/
 void TurnOnGreenLEDAndTurnOffRedLEDs(){
     
-    digitalWrite(3, HIGH);  // Turn on Green LED 1
-    digitalWrite(4, LOW);   // Turn off Red LED 1
-    digitalWrite(5, LOW);   // Turn off Red LED 2
+    digitalWrite(greenLEDPinNum, HIGH);  // Turn on Green LED 1
+    digitalWrite(redLED1PinNum, LOW);   // Turn off Red LED 1
+    digitalWrite(redLED2PinNum, LOW);   // Turn off Red LED 2
     
 }
