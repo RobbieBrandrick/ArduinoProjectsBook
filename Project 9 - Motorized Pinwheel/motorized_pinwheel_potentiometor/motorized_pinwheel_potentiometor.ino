36a1f87a04414c60a9f62eba01195c880bf736ff
Robbie Brandrick <me@robbiebrandrick.com>
2016-03-05 21:04:15 -0500

    Completed project 9 variation

---
+++ Project 9 - Motorized Pinwheel/motorized_pinwheel_potentiometor/motorized_pinwheel_potentiometor.ino
@@ -0,0 +1,24 @@
+/*
+ * Controls a 9v motor via a potentiometer
+ */
+const int motorPin = 9;
+const int potentiometerPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop() {
+
+  int potentiometerValue = analogRead(potentiometerPin);
+  int motorValue = map(potentiometerValue, 0, 1023, 0, 255);
+
+  Serial.print("Potentiometer Value: ");
+  Serial.print(potentiometerValue);
+  Serial.print(" Motor Value: ");
+  Serial.println(motorValue);
+
+  analogWrite(motorPin, motorValue); 
+  
+}
